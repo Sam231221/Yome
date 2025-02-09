2fdd61780eca2f40a20d4aa15a2b032f41a48cde
Sameer Shahi Thakuri (Sam) <70689865+Sam231221@users.noreply.github.com>
2025-02-09 15:52:24 +0545

    🔥 chore(server): Remove EI management features

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import AuthRoutes from "./routes/AuthRoutes.js";
 import MessageRoutes from "./routes/MessageRoutes.js";
 
 import DbRoutes from "./routes/DbRoutes.js";
-import EIRoutes from "./routes/EIRoutes.js";
+
 import { Server } from "socket.io";
 import { v2 as cloudinary } from "cloudinary";
 
@@ -31,8 +31,6 @@ app.use("/uploads/images/", express.static("uploads/images"));
 
 app.use("/api/auth/", AuthRoutes);
 app.use("/api/messages", MessageRoutes);
-
-app.use("/api/ei", EIRoutes);
 app.use("/api/db", DbRoutes);
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
--- server/routes/DbRoutes.js
+++ server/routes/DbRoutes.js
@@ -2,17 +2,13 @@ import { Router } from "express";
 import {
   createMultipleUsersWithProfiles,
   createEducationGroups,
-  createEducationalInstitutions,
   deleteAllRecords,
-  createEducationalUtils,
 } from "../controllers/DbController.js";
 
 const router = Router();
 
 router.get("/create-multiple-users", createMultipleUsersWithProfiles);
 router.get("/create-multiple-groups", createEducationGroups);
-router.get("/create-multiple-eis", createEducationalInstitutions);
-router.get("/create-multiple-ei-utils", createEducationalUtils);
 router.get("/deleteAll", deleteAllRecords);
 
 export default router;
